bfd62941582b53374db644051294ff2c34ccbab1
Caleb <calebgagnon571@gmail.com>
2020-04-23 13:31:54 -0500

    Working version 1.0
    one goal game and squares

--- test.py
+++ test.py
@@ -14,9 +14,14 @@ YELLOW = (255, 255, 0)
 
 #define character values
 CHAR = (50,50)
+BALL = (20,20)
 SPEED = 10
 JUMP = 15
 GRAVITY = 1
+FRICTION = 1
+XBOUNCE = 2
+YBOUNCE = 2
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -64,6 +69,8 @@ class Player1(Player):
             self.xVel = SPEED
         if keystate[pygame.K_w] and self.rect.bottom == HEIGHT - HEIGHT/4:
             self.yVel = -JUMP
+        if keystate[pygame.K_s]:
+            self.yVel = JUMP
 
         super(Player1, self).updatePos()
 
@@ -85,6 +92,8 @@ class Player2(Player):
             self.xVel = SPEED
         if keystate[pygame.K_UP] and self.rect.bottom == HEIGHT - HEIGHT/4:
             self.yVel = -JUMP
+        if keystate[pygame.K_DOWN]:
+            self.yVel = JUMP
 
         super(Player2, self).updatePos()
 
@@ -92,7 +101,7 @@ class Player2(Player):
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface(CHAR, pygame.SRCALPHA)
+        self.image = pygame.Surface(BALL, pygame.SRCALPHA)
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.rect.bottom = HEIGHT/4
@@ -102,23 +111,47 @@ class Ball(pygame.sprite.Sprite):
 
     def update(self):
 
-        if self.rect.bottom < HEIGHT - HEIGHT/4:
+        #yVel
+        if self.yVel > 0:
             self.yVel += GRAVITY
+            self.rect.bottom += self.yVel
+        elif self.yVel < 0:
+            self.yVel += 2*GRAVITY
+            self.rect.bottom += self.yVel
+        elif self.yVel == 0 and self.rect.bottom < HEIGHT - HEIGHT/4:
+            self.yVel = 1
+
+        if  self.rect.bottom >= HEIGHT - HEIGHT/4:
+            self.rect.bottom = HEIGHT - HEIGHT/4
+            self.yVel = -self.yVel
+            if self.xVel > 0:
+                self.xVel -= FRICTION
+            elif self.xVel < 0:
+                self.xVel += FRICTION
+            
+        self.rect.x += self.xVel
         
+        if self.rect.x > WIDTH - 50 or self.rect.x < 0:
+            self.xVel = -self.xVel
 
-        self.rect.bottom += self.yVel
+        
+    def collision(self, player):
+
+        self.xVel = (self.rect.x - player.rect.x)/XBOUNCE
+        self.yVel = (self.rect.bottom + self.rect.y - player.rect.bottom)/YBOUNCE
+        if self.xVel > 15:
+            self.xVel = 15
+        if self.yVel > -15:
+            self.yVel = -15
 
 
-
-
-
-
-
-
-
-
-
-
+    def goal(self):
+        if self.rect.x < WIDTH/16 and self.rect.bottom > HEIGHT/2 or self.rect.x > WIDTH - WIDTH/16 and self.rect.bottom > HEIGHT/2:
+            xVel = 0
+            yVel = 0
+            return True
+        else:
+            return False
 
 #237 is y plsyer can jump
 def main():
@@ -150,15 +183,28 @@ def main():
                 running = False
 
         # Update
+        #all_sprites.update()
+
+        p1 = pygame.sprite.collide_circle(ball, player1)
+        p2 = pygame.sprite.collide_circle(ball, player2)
+
+        if bool(p1):
+            ball.collision(player1)
+        if bool(p2):
+            ball.collision(player2)
+
+        # Update
         all_sprites.update()
 
         # Draw / render
         screen.fill(BLACK)
-        pygame.draw.rect(screen, (50,150,0),(0,HEIGHT - HEIGHT/4, WIDTH, HEIGHT/4))
+        pygame.draw.rect(screen, (50,150,0),(0,HEIGHT - HEIGHT/4, WIDTH, HEIGHT/4))#ground
+        pygame.draw.rect(screen, YELLOW, (0, HEIGHT/2, WIDTH/16, HEIGHT/4)) #left goal
+        pygame.draw.rect(screen, YELLOW, (WIDTH-50, HEIGHT/2, WIDTH/16, HEIGHT/4))#rigth goal
         all_sprites.draw(screen)
         # *after* drawing everything, flip the display
         pygame.display.flip()
-
+        running = not ball.goal()
     pygame.quit()
 
 ########################################################################################
